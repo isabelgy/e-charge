16590627c4e3f1dc63664e2b1b192e0b58dd44f1
isabelgy <17331094+isabelgy@users.noreply.github.com>
2018-04-09 16:30:05 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :stations
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :stations, only: [:index, :show, :update, :create]
+    end
+  end
 end
