6ece02c6ac4596d433b440abacaf1643b14f0995
Wendy Lee <wanderlustwendy@icloud.com>
2018-04-09 17:36:57 +0800

    updated rentals and users controllers

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -1,15 +1,30 @@
 class RentalsController < ActionController::Base
   def create
     @rental = rental.new(rental_params)
+    @station = Station.find(@rental[:station_id])
+    @rental.station = @station
+    @user = User.find(params[:user_id])
+    @rental.user = @user
+    unless @rental.save
+      render_error
+    end
   end
 
   def update
     @rental = rental.find(params[:id])
+    unless @rental.update(rental_params)
+      render_error
+    end
   end
 
   private
 
+  def render_error
+    render json: { errors: @rental.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
   def rental_params
-    params.require(:rental).permit(:in_progress)
+    params.require(:rental).permit(:in_progress, :station_id, :user_id)
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ActionController::Base
+  def create
+    @user = user.new(user_params)
+    unless @user.save
+      render_error
+    end
+  end
+
+  private
+
+  def render_error
+    render json: { errors: @rental.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def user_params
+    params.require(:rental).permit(:name)
+  end
 end
