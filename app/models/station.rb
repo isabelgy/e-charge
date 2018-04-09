b2d7b08de6fc436c1c0a645834008e40f053f4b1
Isabel Chan <isabelchan1994@gmail.com>
2018-04-09 16:53:02 +0800

    seed file completed

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,6 @@
 class Station < ApplicationRecord
-  has_many :rentals
-  has_many :users through :rentals
+  has_many :rentals, dependent: :destroy
+  has_many :users, :through :rentals
 
   validates: :location, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+available? = [false, true]
+
+Station.create(location: '31.230390, 121.473702', availability: available?.sample, photo: 'http://p6.p.pixnet.net/albums/userpics/6/8/1945968/4a22857bc90d0.jpg' )
+Station.create(location: '31.229448, 121.470441', availability: available?.sample, photo: 'https://static.panoramio.com.storage.googleapis.com/photos/large/54044725.jpg' )
+Station.create(location: '31.227466, 121.463488', availability: available?.sample, photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSITql3ye88aNR_Ez-5VcgKjKyO1YmUdD1LZcYgL8loyNY5dbwG
+https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSITql3ye88aNR_Ez-5VcgKjKyO1YmUdD1LZcYgL8loyNY5dbwG' )
+Station.create(location: '31.222989, 121.451300', availability: available?.sample, photo: 'http://farm8.static.flickr.com/7754/16745442014_7a6fc9d211_c.jpg
+http://farm8.static.flickr.com/7754/16745442014_7a6fc9d211_c.jpg' )
 
