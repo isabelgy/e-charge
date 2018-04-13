472fa473a005f16734a80c4f901718a321432b08
Isabel Chan <isabelchan1994@gmail.com>
2018-04-13 13:01:43 +0800

    fixed the user problem

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,58 +1,54 @@
 class Api::V1::UsersController < Api::V1::BaseController
   require 'rest-client'
-  # require 'pry-byebug'
+  require 'pry-byebug'
 
   respond_to :json
 
   URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
 
   def create
-    # puts "create"
+    @user = (User.find_by openid: wechat_email) || User.new(user_wechat_params)
 
-    @user = User.find_by openid: wechat_email || User.create(user_wechat_params)
-    puts @user.id
-    unless @user.save
-      render_error
-    end
+    # unless
+     @user.save
+
+    #  puts @user.openid
+    # puts @user.password
+    # puts @user.id
+      # render_error
+    # end
     render json: @user if @user.persisted?
   end
 
   private
 
-  def render_error
-    render json: { errors: @rental.errors.full_messages },
-      status: :unprocessable_entity
-  end
-
-  def wechat_email
-   @wechat_email ||= wechat_user.fetch('openid') + "@myapp.com"
-  end
-  # def wechat_openid
-  #   puts "openid"
-  #   wechat_user.fetch("openid")
+  # def render_error
+  #   render json: { errors: @rental.errors.full_messages },
+  #     status: :unprocessable_entity
   # end
 
-  def user_wechat_params
-    # return @user_wechat_params if @user_wechat_params
+  def wechat_email
 
-# binding.pry
-    # @user_wechat_params = user_params
-    @user_wechat_params = {}
+   @wechat_email ||= wechat_user.fetch('openid') + "@myapp.com"
+
+  end
+
+  def user_wechat_params
+    @user_wechat_params ||= {}
+
    # GET both openid and session_key
+    # @user_wechat_params = user_params
     @user_wechat_params['openid'] = wechat_email
-    @user_wechat_params['password'] = wechat_user.fetch('session_key')#, Devise.friendly_token)
-    # @user_wechat_params['authentication_token'] = @user_wechat_params['password']
+    @user_wechat_params['password'] = wechat_user.fetch('session_key')
     @user_wechat_params
   end
 
 
   def wechat_user
-    # puts "wechatuser"
 
     @wechat_response ||= RestClient.post(URL, wechat_params)
-    # puts "got wechat_response"
-    # puts @wechat_response
     @wechat_user ||= JSON.parse(@wechat_response.body)
+    @wechat_user
   end
 
   def wechat_params
@@ -63,7 +59,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :openid, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :stations, :through => :rentals
   has_many :rentals, dependent: :destroy
+  # validates :password, presence: true
+  # validates :openid, presence: true
   # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
